4f249fcecd25aa71657858ed75fdac4b1cd6cafa
Tenari the Great <djz24@case.edu>
2013-08-26 14:35:44 -0400

    HTML: start: current orders accepted by chefs

--- client/js/chef_template.js
+++ client/js/chef_template.js
@@ -4,6 +4,9 @@ Template.chef.orders = function(){
 Template.chef.open = function(id){
   return Orders.find(id).fetch()[0].taker == "noneyet";
 };
+Template.chef.own_order = function(id){
+  return Orders.find(id).fetch()[0].taker == Meteor.user()._id;
+};
 Template.chef.events({
   'click .accept': function(e){
     Orders.update($(e.target).data('id'), {$set: {taker: Meteor.userId()}});
