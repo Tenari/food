eb2ce6e83760b3184b79381676df6b7c6ca554c2
Tenari the Great <djz24@case.edu>
2013-08-27 17:44:49 -0400

    HUNGRY: add: order-link functionality

--- client/js/hungryorders_template.js
+++ client/js/hungryorders_template.js
@@ -1,3 +1,25 @@
 Template.hungryorders.orders = function(){
   return Orders.find({placer: Meteor.user()._id}).fetch();
 };
+Template.hungryorders.events({
+  'click .order-link': function(e){
+    //clear all the old chef infos
+    $('.chef-info').remove();
+
+    var order_id = $(e.target).attr("id");
+    var taker_id = Orders.find(order_id).fetch()[0].taker;
+    var taker_obj = Meteor.users.find(taker_id).fetch()[0];
+    var $container = $(e.target).parent();
+
+    if (Session.get('chef-info') != order_id){
+      if(taker_obj != undefined){
+        $container.append("<p class='chef-info'><strong>Chef Name:</strong>"+taker_obj.username+"</p>");
+      } else{
+        $container.append("<p class='chef-info'><strong>Chef Name:</strong> -- </p>");
+      }
+      Session.set('chef-info', order_id);
+    }else{
+      Session.set('chef-info', undefined);
+    }
+  }
+});
