ee04f23dad9459eb25ca2c7277498dab6fa92310
Tenari the Great <djz24@case.edu>
2013-08-26 22:28:40 -0400

    CLIENT: edit: show current orders functionality

--- client/js/chef_template.js
+++ client/js/chef_template.js
@@ -7,11 +7,21 @@ Template.chef.open = function(id){
 Template.chef.own_order = function(id){
   return Orders.find(id).fetch()[0].taker == Meteor.user()._id;
 };
+Template.chef.show_current_orders = function(){
+  return Session.get('show-current-orders') == true;
+};
 Template.chef.events({
   'click .accept': function(e){
     Orders.update($(e.target).data('id'), {$set: {taker: Meteor.userId()}});
   },
   'click .remove': function(e){
     $(e.target).parent().parent().remove();
+  },
+  'click #show-current-orders': function(e){
+    if (Session.get('show-current-orders')){
+      Session.set('show-current-orders', false);
+    }else{
+      Session.set('show-current-orders', true);
+    }
   }
 });
