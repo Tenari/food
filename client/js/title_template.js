92b25cea8c72ab03b13b20513008eba409b5e54b
Tenari the Great <djz24@case.edu>
2013-08-27 13:27:25 -0400

    HUNGRY: title matches view

--- client/js/title_template.js
+++ client/js/title_template.js
@@ -1,5 +1,8 @@
 Template.title.helpers({
   hungry: function(){
     return Meteor.user().profile.hungry;
-  }  
+  },  
+  no_orders: function(){
+    return Orders.find({placer: Meteor.user()._id, finished: "nottaken"}).fetch().length == 0;
+  }
 });
