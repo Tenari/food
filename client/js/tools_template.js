a68689fa51080f3523fd880eece6eceea1ef1496
Tenari <djz24@case.edu>
2013-09-02 15:48:01 -0400

    Refactor: tools button

--- client/js/tools_template.js
+++ client/js/tools_template.js
@@ -1,9 +1,17 @@
+Template.tools.showTools = function(){
+  return Session.get('showTools');
+};
 Template.tools.events({
   'click input' : function () {
-    // template data, if any, is available in 'this'
-    Meteor.effects.showTools();
+    if(Session.get('showTools'))
+      Session.set('showTools', false);
+    else
+      Session.set('showTools', true);
   },
   'click #logout': function(){
     Meteor.logout();
   }
 });
+Template.tools.rendered = function(){
+  $('.tools ul').hide().fadeIn();
+}
